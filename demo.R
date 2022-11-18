d6dc562fb2a7718711388289d8e38a67938f30d0
merigo <dmerigos@gmail.com>
2022-11-18 12:50:25 -0800

    Added arg4 optional positional argument to demo.R

--- demo.R
+++ demo.R
@@ -7,7 +7,9 @@ Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
+--arg4=<arg4>     Takes any value (this is an optional option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+
 " -> doc
 
 library(docopt)
